0c40db740ef8054cc97c8e7de7285e342ec7b42e
automatypaulina-hue <automatypaulina@gmail.com>
2026-02-21 15:08:54 +0100

    logowanie

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from selenium.webdriver import Chrome
 from time import sleep
+from selenium.webdriver.common.by import By
+DATA_EMAIL = "kowalski@spam.pl"
 
 #stworzenie instalacji klasy Chrome
 #(to otworzy przegladarke)
 driver = Chrome()
 #Otwarcie strony
-driver.get("https://www.kozminski.edu.pl/pl")
+driver.get("https://automationpractice.techwithjatin.com/")
 #Maksymalizacja okna
 driver.maximize_window()
-sleep(5)
-driver.quit()
+
+#Znajdzelement Sign in
+sign_in_a = driver.find_element(By.PARTIAL_LINK_TEXT, "Sign in")
+#kliknij w odnaleziony element
+sign_in_a.click()
+driver.find_element(By.ID, "email_create").send_keys(DATA_EMAIL)
+
+print(type(sign_in_a))
+sleep(3)
+
+
+
 
