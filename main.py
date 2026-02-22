b36d45d7f6f5e1be7778ab1378ab9e55c76d0910
automatypaulina-hue <automatypaulina@gmail.com>
2026-02-22 09:56:44 +0100

    zmiany

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
+from itertools import dropwhile
+
 from selenium.webdriver import Chrome
 from time import sleep
 from selenium.webdriver.common.by import By
-DATA_EMAIL = "kowalski@spam.pl"
 
-#stworzenie instalacji klasy Chrome
-#(to otworzy przegladarke)
+
+class Gender:
+    MALE = 0
+    FEMALE = 1
+
+# Dane testowe
+DATA_EMAIL = "kdhfsjdh@spam.la"
+DATA_GENDER = Gender.MALE
+
+# Stworzenie instancji klasy Chrome
+# (to otworzy przeglądarkę)
 driver = Chrome()
-#Otwarcie strony
+# Otwarcie strony
 driver.get("https://automationpractice.techwithjatin.com/")
-#Maksymalizacja okna
-driver.maximize_window()
 
-#Znajdzelement Sign in
+# Maksymalizacja okna
+driver.maximize_window()
+# Znajdz element Sign in
 sign_in_a = driver.find_element(By.PARTIAL_LINK_TEXT, "Sign in")
-#kliknij w odnaleziony element
+# Kliknij w odnaleziony element
 sign_in_a.click()
+
+# Wpisz email
 driver.find_element(By.ID, "email_create").send_keys(DATA_EMAIL)
 
-print(type(sign_in_a))
-sleep(3)
+# Kliknij Create Account
+driver.find_element(By.ID, "SubmitCreate").click()
+
+driver.implicitly_wait(5)
+# Kliknij swoją płeć
+if DATA_GENDER == Gender.FEMALE:
+    driver.find_element(By.XPATH, '//label[@for="id_gender2"]').click()
+
+else:
+    driver.find_element(By.XPATH, '//label[@for="id_gender1"]').click()
+
+sleep(5)
+driver.quit()
 
 
 
